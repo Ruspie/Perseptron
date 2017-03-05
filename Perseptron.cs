43dfbfed17368614a7c91a21e9def5c6105a2236
Alex_Begun <begun4ik111@mail.ru>
2017-03-05 15:41:01 +0300

    Добавьте файлы проекта.

---
+++ Perseptron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perseptron
+{
+    public class Perseptron
+    {
+        private readonly int _numberClasses;
+        private readonly int _vectorSize;
+
+        public Perseptron(int numberClasses, int vectorSize)
+        {
+            _numberClasses = numberClasses;
+            _vectorSize = vectorSize;
+        }
+    }
+}
