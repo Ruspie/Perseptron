2e76f9594a4a2b76dd074e742e0afd2099c347d5
Alex_Begun <begun4ik111@mail.ru>
2017-03-12 17:28:50 +0300

    Final correct implementation Perseptron method, create Writer result

--- Perseptron.cs
+++ Perseptron.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Perseptron
 {
@@ -12,10 +13,14 @@ namespace Perseptron
         {
             Function[] functions = InitializeFunctions(numberClasses, vectorSize);
             bool isError;
+            int numberIteration = 0;
 
             do {
                 isError = DoPerseptronIteration(teachingVectors, functions, numberClasses);
-            } while (isError);
+                numberIteration++;
+            } while (isError && numberIteration != 1000);
+
+            if (isError) ShowErrorMessage();
 
             return functions;
         }
@@ -32,16 +37,17 @@ namespace Perseptron
         private static bool DoPerseptronIteration(Vector[][] teachingVectors, Function[] functions, int numberClasses)
         {
             bool isError = false;
-
             for (int classNumber = 0; classNumber < numberClasses; classNumber++) {
                 for (int vectorNumber = 0; vectorNumber < teachingVectors[classNumber].Length; vectorNumber++) {
-                    int currentClassNumber = GetNumberClassForVector(functions, teachingVectors[classNumber][vectorNumber]);
+                    int currentClassNumber = ClassifyVectorToClass(functions,
+                        teachingVectors[classNumber][vectorNumber]);
                     if (currentClassNumber != classNumber) {
                         MakeAdjustment(teachingVectors[classNumber][vectorNumber], functions, classNumber);
                         isError = true;
                     }
                 }
             }
+
             return isError;
         }
 
@@ -55,7 +61,7 @@ namespace Perseptron
             }
         }
 
-        private static int GetNumberClassForVector(Function[] functions, Vector vector)
+        public static int ClassifyVectorToClass(Function[] functions, Vector vector)
         {
             int maxValue = int.MinValue;
             int classNumber = int.MinValue;
@@ -71,6 +77,12 @@ namespace Perseptron
                 }
             }
             return countSameResult == 1 ? classNumber : -1;
+        }
+
+        private static void ShowErrorMessage()
+        {
+            MessageBox.Show(@"The iteration cycle does not converge, class definition errors are possible",
+                       @"Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
     }
 }
---
+++ Writer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perseptron;
+
+namespace Perseptron
+{
+    public static class Writer
+    {
+        public static string PrintVectors(Vector[][] vectors)
+        {
+            string result = "";
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                result += @"Vectors " + (i + 1) + @" class :" + "\r\n";
+                for (int j = 0; j < vectors[i].Length; j++)
+                {
+                    result += (j + 1) + @" vectors : (";
+                    for (int k = 0; k < vectors[i][j].GetElementsLength() - 2; k++)
+                    {
+                        result += vectors[i][j].GetElement(k) + @", ";
+                    }
+                    result += vectors[i][j].GetElement(vectors[i][j].GetElementsLength() - 2) + @");" + "\r\n";
+                }
+                result += "\r\n";
+            }
+            result = result.Remove(result.Length - 4, 4);
+
+            return result;
+        }
+
+        public static string PrintFunctions(Function[] functions)
+        {
+            string result = "";
+
+            for (int i = 0; i < functions.Length; i++)
+            {
+                result += @"d(" + (i + 1) + @") = ";
+                for (int j = 0; j < functions[i].GetElementsLength() - 1; j++)
+                {
+                    if (j != 0 && functions[i].GetElement(j) >= 0)
+                        result += @"+";
+                    result += functions[i].GetElement(j) + "*x" + (j + 1);
+                }
+                if (functions[i].GetElement(functions[i].GetElementsLength() - 1) >= 0)
+                    result += "+";
+                result += functions[i].GetElement(functions[i].GetElementsLength() - 1) + ";" + "\r\n";
+            }
+            result = result.Remove(result.Length - 2, 2);
+
+            return result;
+        }
+    }
+}
